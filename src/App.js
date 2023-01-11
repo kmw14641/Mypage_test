0340e7c64842859cadc69b08bf8706cd6de3589b
kmw14641 <102219400+kmw14641@users.noreply.github.com>
2023-01-11 22:49:36 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import {Route, Routes} from 'react-router-dom';
 import Home from './home.js';
-import About from './about.js';
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/about' element={<About/>}></Route>
-      </Routes>
+      <Home/>
     </div>
   );
 }
--- src/about.js
+++ src/about.js
@@ -1,4 +1,4 @@
-const about = () => {
+const About = () => {
     return (
         <div>
             <h1>This is about.</h1>
@@ -6,4 +6,4 @@ const about = () => {
     )
 }
 
-export default about;
+export default About;
